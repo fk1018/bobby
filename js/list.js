98f4c26af88a0a65abd6da5403794f0f7fb0996f
Adams <baadams@baadams-aa5c03.lowes.com>
2016-11-21 08:39:59 -0500

    removed numbers

--- js/list.js
+++ js/list.js
@@ -2,32 +2,23 @@
     window.Lowes = Lowes || {}
 
     window.Lowes.list = {
-<<<<<<< HEAD
             createProductDiv:function(){
                 console.log();
 
             },
-=======
-        createProductDiv:function(product){
-            console.log(product);
-        },
->>>>>>> 441d59e0083673d1a2d6ec78eccf02358b9bec77
         getProducts:function(){
             $.ajax({
                 type:'GET',
                 url:'https://m.lowes.com/IntegrationServices/resources/productList/json/v3_0/4294857975?langId=-1&storeId=10702&catalogId=10051&nValue=4294857975&storeNumber=0595&pageSize=20&firstRecord=0',
+
             }).done(function(data){
                window.Lowes.list.productList = data.ProductsList
                window.Lowes.list.populatePLDivs()
-<<<<<<< HEAD
                window.Lowes.list.populateTopDiv()
-=======
->>>>>>> 441d59e0083673d1a2d6ec78eccf02358b9bec77
             })
         },
         init:function(){
             this.getProducts()
-<<<<<<< HEAD
             this.populatePLDivs()
             this.populateTopDiv()
         },
@@ -65,16 +56,5 @@
 
 
 
-=======
-        },
-        productList:[],
-        productListDivs:[],
-        populatePLDivs:function(){
-            var pl = this.productList
-
-            for(product in pl){
-                $('body').append('<div class="col-md-4 col-lg-4 col-sm-12 col-xs-12"><img src="'+pl[product].imageURLs.lg+'"></div>')            }
-            }
->>>>>>> 441d59e0083673d1a2d6ec78eccf02358b9bec77
     }
 })(window.Lowes,jQuery)

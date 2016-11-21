853dcbfe6aa6a3fcb82f2a99b6737d136dc6b288
Fredrick Khoury <fredrick.f.khoury@lowes.com>
2016-11-21 11:01:05 -0500

    zz

--- js/list.js
+++ js/list.js
@@ -1,68 +1,68 @@
-(function(Lowes,$){
+(function(Lowes, $) {
     window.Lowes = Lowes || {}
 
     window.Lowes.list = {
-            createProductDiv:function(){
-                console.log();
+        createProductDiv: function() {
+            console.log();
 
-            },
-        getProducts:function(){
+        },
+        getProducts: function() {
             $.ajax({
-                type:'GET',
-                url:'https://m.lowes.com/IntegrationServices/resources/productList/json/v3_0/4294857975?langId=-1&storeId=10702&catalogId=10051&nValue=4294857975&storeNumber=0595&pageSize=20&firstRecord=0',
+                type: 'GET',
+                url: 'https://m.lowes.com/IntegrationServices/resources/productList/json/v3_0/4294857975?langId=-1&storeId=10702&catalogId=10051&nValue=4294857975&storeNumber=0595&pageSize=20&firstRecord=0',
 
-            }).done(function(data){
-               window.Lowes.list.productList = data.ProductsList
-               window.Lowes.list.populatePLDivs()
-               
+            }).done(function(data) {
+                window.Lowes.list.productList = data.ProductsList
+                window.Lowes.list.populatePLDivs()
+
             })
         },
-        init:function(){
+        init: function() {
             this.getProducts()
             this.populatePLDivs()
-            
+
         },
-        productList:[],
-        productListDiv:[],
-        populatePLDivs:function(){
-               var pl = this.productList;
-            	var count = 1;
+        productList: [],
+        productListDiv: [],
+        populatePLDivs: function() {
+            var pl = this.productList;
+            var count = 1;
 
-               for(product in pl){
-                    $('body').append('<div id = '+count+'><div class="col-md-4 col-lg-4 col-sm-12 col-xs-12"><img src="'+pl[product].imageURLs.sm+'"> '+ pl[product].ProductInfo.Brand + " " + pl[product].ProductInfo.p_product_description + "$" + pl[product].ProductInfo.p_product_price+' </div>')
-                  count++
-                    //$('body').append('<div class="col-md-4 col-lg-4 col-sm-12 col-xs-12"> '+pl[product].ProductInfo.Brand +'</div>')
-                    //$('body').append(pl[product].ProductInfo.p_product_description)
-                    //$('body').append("$" +pl[product].ProductInfo.p_product_price)
+            for (var product in pl) {
+                $('body').append('<div id = ' + count + '><div class="col-md-4 col-lg-4 col-sm-12 col-xs-12"><img src="' + pl[product].imageURLs.sm + '"> ' + pl[product].ProductInfo.Brand + " " + pl[product].ProductInfo.p_product_description + "$" + pl[product].ProductInfo.p_product_price + ' </div>')
 
-                    //var btn = document.createElement("BUTTON");        
-                    //var t = document.createTextNode("View More");       
-                    //btn.appendChild(t);                                 
-                    //document.body.appendChild(btn);
- 					//$('#header').append('<img src="'+pl[product].imageURLs.sm+'">')
-  
-               }
-               
-               $(this).attr('div').hover(function(){
-			        //$(this).css("background-color", "yellow");
-			        alert("working");
-			        }, function(){
-			        //$(this).css("background-color", "pink");
-			        alert("not working");
-			    });
-               $('#header').append('<img src="'+pl[product].imageURLs.lg+'">')
-               $('#header').append(pl[product].ProductInfo.p_product_bullets_json.Value)
-               //.css("list-style-type, "bullet");
-               $('#header').append("$" +pl[product].ProductInfo.p_product_price)
-               var btn = document.createElement("BUTTON");        
-               var t = document.createTextNode("Add to Cart");       
-               btn.appendChild(t);                                 
-               document.body.appendChild(btn);
+                //$('body').append('<div class="col-md-4 col-lg-4 col-sm-12 col-xs-12"> '+pl[product].ProductInfo.Brand +'</div>')
+                //$('body').append(pl[product].ProductInfo.p_product_description)
+                //$('body').append("$" +pl[product].ProductInfo.p_product_price)
+
+                //var btn = document.createElement("BUTTON");        
+                //var t = document.createTextNode("View More");       
+                //btn.appendChild(t);                                 
+                //document.body.appendChild(btn);
+                //$('#header').append('<img src="'+pl[product].imageURLs.sm+'">')
+                $('#' + count).hover(function() {
+                    //$(this).css("background-color", "yellow");
+                    alert("working");
+                }, function() {
+                    //$(this).css("background-color", "pink");
+                    alert("not working");
+                });
+            }
+
+
+            /*$('#header').append('<img src="' + pl[product].imageURLs.lg + '">')
+            $('#header').append(pl[product].ProductInfo.p_product_bullets_json.Value)
+                //.css("list-style-type, "bullet");
+            $('#header').append("$" + pl[product].ProductInfo.p_product_price)
+            var btn = document.createElement("BUTTON");
+            var t = document.createTextNode("Add to Cart");
+            btn.appendChild(t);
+            document.body.appendChild(btn);*/
 
         }
-    
+
 
 
 
     }
-})(window.Lowes,jQuery)
+})(window.Lowes, jQuery)
